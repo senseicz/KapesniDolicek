ec3d0136ac78e2cea7b8b4a80e652101a9ed869d
Antonin Jelinek <antonin.jelinek@gmail.com>
2015-12-07 17:19:16 +0100

    updates

--- www/app/app.js
+++ www/app/app.js
@@ -1,41 +1,40 @@
 // Ionic Starter App
-angular.module('KapesniDolicek', ['ionic',
-    'angular-data.DSCacheFactory',
-    'KapesniDolicek.directives',
-    'KapesniDolicek.filters',
-    
-])
+angular.module('KapesniDolicek', [
+        'ionic',
+        'ngCordova',
+        'angular-data.DSCacheFactory',
+        'KapesniDolicek.directives',
+        'KapesniDolicek.filters',
+    ])
+    .constant('BaseUrl', 'http://www.bohemians.cz')
+    .constant('ApiEndpoint', 'http://www.bohemians.cz/api')
+    .run(function($ionicPlatform, $ionicHistory, DSCacheFactory) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            //if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            //    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            //    cordova.plugins.Keyboard.disableScroll(true);
+            //}
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
 
-.constant('BaseUrl', 'http://www.bohemians.cz')
-.constant('ApiEndpoint', 'http://www.bohemians.cz/api')
+            DSCacheFactory("appCache", { storageMode: "localStorage" });
+            DSCacheFactory("staticCache", { storageMode: "localStorage" });
 
-.run(function ($ionicPlatform, $ionicHistory, DSCacheFactory) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
+        });
 
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
-
-        //DSCacheFactory("leagueDataCache", { storageMode: "localStorage", maxAge: 360000, deleteOnExpire: "aggressive" });
-        DSCacheFactory("appCache", { storageMode: "localStorage" });
-        DSCacheFactory("staticCache", { storageMode: "localStorage" });
-
-    });
-
-    $ionicPlatform.registerBackButtonAction(function (evt) {
-        if (evt && evt.type == 'backclick') {
-            $ionicHistory.goBack();
-            return true;
-        }
-    }, 100);
-})
+        $ionicPlatform.registerBackButtonAction(function(evt) {
+            if (evt && evt.type == 'backclick') {
+                $ionicHistory.goBack();
+                return true;
+            }
+        }, 100);
+    })
+    .factory('KapesniDolicek', ['$http', '$q', '$ionicLoading', '$timeout', 'DSCacheFactory'], function($http, $q, $ionicLoading, $timeout, DSCacheFactory) {
+    })
 
 .config(function ($stateProvider, $urlRouterProvider, $compileProvider) {
 
